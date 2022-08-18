a731321d9f7c13a0941715db19065047683a0187
AndreASousa <86360743+AndreASousa@users.noreply.github.com>
2022-08-18 09:40:29 -0300

    Add files via upload

--- 004 - Indicadores_Socioeconomicos.R
+++ 004 - Indicadores_Socioeconomicos.R
@@ -25,27 +25,29 @@ rm(pacotes)
 #                        Extração de dados do IBGE                             #
 ################################################################################
 
-#As informações quanto ao API do IBGE podem ser acessadas em:
-#https://servicodados.ibge.gov.br/api/docs/localidades#api-Municipios
+# As informações quanto ao API do IBGE podem ser acessadas em:
+# https://servicodados.ibge.gov.br/api/docs/agregados?versao=3#api-bq
 
-#1 - Extraindo o conjunto de municípios do estado de Sâo Paulo
-url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/35/municipios"
+#1 - População residente estimada
+url = "https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/2001|2002|2003|2004|2005|2006|2007|2008|2009|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020|2021/variaveis/9324?localidades=N6[N3[35]]"
 
-municipios_sp <- GET(url) |>
+populacao <- GET(url) |>
   getElement("content") |> #Selecionando "$content"
   rawToChar() |> #Transforma o conteúdo em um texto
   iconv(to = "latin1//TRANSLIT", from = "UTF-8") |>#Modifica a codificação para "UTF-8"
-  fromJSON()|>  #Converte objetos jason para "r"
-  select(id, nome)
+  fromJSON()  #Converte objetos jason para "r"
 
-save(municipios_sp, file="Documentos/Municipios_SP.RData")
+populacao <- populacao[[4]][[1]][[2]][[1]]
+populacao <- cbind(populacao$localidade$id, populacao$serie)
+populacao <- rename(populacao, "id" = "populacao$localidade$id")
 
-#2 - Extraindo o conjunto de distritos do município de Sâo Paulo
-url = "https://servicodados.ibge.gov.br/api/v1/localidades/municipios/3550308/distritos"
-sao_paulo <- GET(url) |>
+save(populacao, file="Documentos/populacao.RData")
+
+#2 - PIB
+url = "https://servicodados.ibge.gov.br/api/v3/agregados/5938/periodos/2002|2003|2004|2005|2006|2007|2008|2009|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019/variaveis/37|513|517|6575|525?localidades=N6[N3[35]]"
+
+pib <- GET(url) |>
   getElement("content") |> #Selecionando "$content"
   rawToChar() |> #Transforma o conteúdo em um texto
   iconv(to = "latin1//TRANSLIT", from = "UTF-8") |>#Modifica a codificação para "UTF-8"
   fromJSON()  #Converte objetos jason para "r"  
-
-save(sao_paulo, file="Documentos/Municipio_SP.RData")
