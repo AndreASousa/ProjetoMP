5dcb88864e5c64275fc2f4ccfe35b9d23242ad9b
AndreASousa <86360743+AndreASousa@users.noreply.github.com>
2022-08-26 17:02:31 -0300

    Add files via upload

--- 004 - Indicadores_Socioeconomicos.R
+++ 004 - Indicadores_Socioeconomicos.R
@@ -1,6 +1,6 @@
 
-## As informações sócioeconômicas extraidas a seguir poderão ser conectadas
-  # ao restante do banco de dados pelo código de identificação do IBGE
+## O Script se destina à criaçao de uma tabela que agregue informações
+  # socioeconômicas de cada Comarca paulista, a cada ano.
 
 ################################################################################
 #                 INSTALAÇÃO E CARREGAMENTO DE PACOTES NECESSÁRIOS             #
@@ -10,7 +10,8 @@ pacotes <- c("here",
              "rvest", #Faz o Webscraping
              "httr", 
              "jsonlite",
-             "tidyverse")
+             "tidyverse",
+             "readxl")
 
 if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
   instalador <- pacotes[!pacotes %in% installed.packages()]
@@ -25,14 +26,14 @@ if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
 rm(pacotes)
 
 ################################################################################
-#                        Extração de dados do IBGE                             #
+#                        Extração de Dados Socioeconômicos                     #
 ################################################################################
 
 # As informações quanto ao API do IBGE podem ser acessadas em:
 # https://servicodados.ibge.gov.br/api/docs/agregados?versao=3#api-bq
 
-#1 - População residente estimada
-url  <-  "https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/2001|2002|2003|2004|2005|2006|2007|2008|2009|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020|2021/variaveis/9324?localidades=N6[N3[35]]"
+#1 - População residente (Não compreende 2007 e 2010)
+url  <-  "https://servicodados.ibge.gov.br/api/v3/agregados/6579/periodos/2001|2002|2003|2004|2005|2006|2008|2009|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020|2021/variaveis/9324?localidades=N6[N3[35]]"
 
 populacao <- GET(url) |>
   getElement("content") |> #Selecionando "$content"
@@ -40,12 +41,16 @@ populacao <- GET(url) |>
   iconv(to = "latin1//TRANSLIT", from = "UTF-8") |>#Modifica a codificação para "UTF-8"
   fromJSON()  #Converte objetos jason para "r"
 
+
+# Criamos uma função para facilitar a extração da lista aninhada resultante da
+  # raspagem de dados
 extrator <- function(objeto = NULL){
   
   extraido <- objeto[[4]][[1]][[2]][[1]]
   extraido <- cbind(extraido$localidade$id, extraido$serie)
   
-  if(!is.null(extraido$localidade$id)){
+  #Renomeamos a coluna qye contém o código IBGE como "id"
+  if("extraido$localidade$id" %in% colnames(extraido)){
     extraido <- rename(extraido, "id" = "extraido$localidade$id")
     }
   
@@ -54,10 +59,63 @@ extrator <- function(objeto = NULL){
 
 populacao <- extrator(populacao)
 
+#2 - População Residente - Censo 2010
+url  <-  "https://servicodados.ibge.gov.br/api/v3/agregados/202/periodos/2010/variaveis/93?localidades=N6[N3[35]]&classificacao=2[0]|1[0]"
+
+populacao_2010 <- GET(url) |>
+  getElement("content") |> #Selecionando "$content"
+  rawToChar() |> #Transforma o conteúdo em um texto
+  iconv(to = "latin1//TRANSLIT", from = "UTF-8") |>#Modifica a codificação para "UTF-8"
+  fromJSON()|>  #Converte objetos jason para "r"
+  extrator()
+
+populacao <- left_join(populacao, populacao_2010, by = "id")
+rm(populacao_2010)
+
+#3 - Contagem Populacional 2007
+url  <-  "https://servicodados.ibge.gov.br/api/v3/agregados/793/periodos/2007/variaveis/93?localidades=N6[N3[35]]"
+
+populacao_2007 <- GET(url) |>
+  getElement("content") |> #Selecionando "$content"
+  rawToChar() |> #Transforma o conteúdo em um texto
+  iconv(to = "latin1//TRANSLIT", from = "UTF-8") |>#Modifica a codificação para "UTF-8"
+  fromJSON()|>  #Converte objetos jason para "r"
+  extrator()
+
+populacao <- left_join(populacao, populacao_2007, by = "id")
+rm(populacao_2007)
+
+# Verificando a consisTência dos dados
+filter_all(populacao, any_vars(is.na(.)))
+
+# Vamos Converter os dados para a estrutura de uma série temporal
+# As observações de cada ano serão dispostas em uma mesma linha, ao invés de colunas 
+
+organiza <- function(objeto = NULL){
+  
+  # Capturando o nome do objeto submetido à função
+  nome_objeto <-deparse(substitute(objeto))
+  
+  objeto <- objeto |>
+    pivot_longer(cols = !id, names_to = "ano",
+                 # A coluna criada recebe o nome do objeto
+                 values_to = nome_objeto) |>
+    arrange(ano, id)
+  
+  # Converte para valores numéricos
+  var_classe <- c("ano", nome_objeto)
+  objeto[var_classe] <- lapply(objeto[var_classe], as.numeric)
+  
+  return(objeto)
+}
+
+populacao <- organiza(populacao)
+
 save(populacao, file="Documentos/populacao.RData")
 
-#2 - PIB a preços correntes
-url  <-  "https://servicodados.ibge.gov.br/api/v3/agregados/5938/periodos/2010|2011|2012|2013|2014|2015|2016|2017|2018|2019/variaveis/37?localidades=N6[N3[35]]"
+
+#4 - PIB Municipal a preços correntes (Série de 2002 a 2019)
+url  <-  "https://servicodados.ibge.gov.br/api/v3/agregados/5938/periodos/2002|2003|2004|2005|2006|2007|2008|2009|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019/variaveis/37?localidades=N6[N3[35]]"
 
 pib <- GET(url) |>
   getElement("content") |>
@@ -66,9 +124,12 @@ pib <- GET(url) |>
   fromJSON()|>
   extrator()
 
+
+pib <- organiza(pib)
+
 save(pib, file="Documentos/pib.RData")
 
-#3 - Total de pessoas ocupadas
+#5 - Total de pessoas ocupadas
 url <- "https://servicodados.ibge.gov.br/api/v3/agregados/1685/periodos/2006|2007|2008|2009|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020/variaveis/707?localidades=N6[N3[35]]"
 
 ocupados <-  GET(url) |>
@@ -78,9 +139,11 @@ ocupados <-  GET(url) |>
   fromJSON() |>
   extrator()
 
+ocupados <- organiza(ocupados)
+
 save(ocupados, file="Documentos/ocupados.RData")
 
-#4 - Salário médio mensal
+#6 - Salário médio mensal
 url <- "https://servicodados.ibge.gov.br/api/v3/agregados/1685/periodos/2006|2007|2008|2009|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020/variaveis/707?localidades=N6[N3[35]]"
 
 salario <-  GET(url) |>
@@ -90,11 +153,11 @@ salario <-  GET(url) |>
   fromJSON() |>
   extrator()
 
-
+salario <- organiza(salario)
 
 save(salario, file="Documentos/salario.RData")
 
-#5 - IDH Municipios 2010
+#7 - IDH Municipios 2010
 url  <-  "https://servicodados.ibge.gov.br/api/v1/pesquisas/37/periodos/2010/indicadores/30255/resultados/35xxxx?scope=sub&pt"
 
 idh <- GET(url) |>
@@ -107,16 +170,34 @@ idh <- idh[[2]][[1]]
 idh <- cbind(idh[1], idh$res$'2010')
 idh$idh <- idh$`idh$res$"2010"`
 idh$`idh$res$"2010"` <- NULL
+idh$idh <- as.numeric(idh$idh)
+
+# Os códigos de Município não correspondem exatamente aos do IBGE
+# Falta o último dígito
+load(here::here("Documentos", "municipios_sp.RData"))
+
+municipios_sp$codigo <- str_sub(municipios_sp$id, end = 6)
+
+# A exclusão do último dígito não gera duplicidade de dados
+length(unique(municipios_sp$codigo))
+
+idh <- idh |>
+  left_join(municipios_sp[ , c("id", "codigo")], by = c("localidade" = "codigo")) |>
+  select(id, idh)
+
+# O procedimento conseguiu vincluar todas as informações ao identificador do IBGE
+count(filter(etario, is.na(id)))
 
 save(idh, file="Documentos/idh.RData")
 
-#6 - Faixas Estárias
+#8 - Faixas Estárias
+
   # Utilizaremos as estimativas do Ministério da Saúde e disponíveis em:
   # http://tabnet.datasus.gov.br/cgi/tabcgi.exe?popsvs/cnv/popbr.def
   # Não foi possível a extrassão automatizada dos dados
   # Extraimos as informações referentes aos anos de 2010 e 2021 em arquivos "csv"
 
-etario <- list.files("Documentos", pattern = "csv$",
+etario <- list.files("Documentos/Estimativa_Etaria", pattern = "csv$",
                                  recursive = T, full.names = T)
 
 # Verificando a ordem em que cada lista foi baixada
@@ -131,66 +212,109 @@ etario <- lapply(etario, read.csv, sep = ";")
 
 etario <- bind_rows(etario)
 
-nrow(etario) / 645 
+nrow(etario) / 645
+
+# Vamos vincular cada observação ao respectivo ano
+etario$ano <- as.character(rep(2006:2021, each = 645))
 
 # Os códigos de Município não correspondem exatamente aos do IBGE
 # Falta o último dígito
-load(here::here("Documentos", "codigos_sp.RData"))
 
 etario$codigo <- str_sub(etario$Município, end = 6)
 
-municipios_sp$codigo <- str_sub(municipios_sp$id, end = 6)
-
+# A exclusão do último dígito não gera duplicidade de dados
 length(unique(municipios_sp$codigo))
 
 etario <- left_join(etario, municipios_sp[ , c("id", "codigo")], by = "codigo") |>
           select(id, !c(codigo, Município))
 
+# O procedimento conseguiu vincluar todas as informações ao identificador do IBGE
 count(filter(etario, is.na(id)))
+
+# Existe divergência entre população residente total estimada pelo IBGE e
+ # pelo Ministério da Saúde. Optamos pelos dados do IBGE.
+etario <- etario|>
+  arrange(ano, id) |>
+  select(id, ano, everything(), -Total)
+
+colnames(etario) <- c("id", "ano", "0a4", "5a9", "10a14", "15a19", "20a24",
+                      "25a29", "30a34", "35a39", "40a44", "45a49", "50a54",
+                      "55a59", "60a64", "65a69", "70a74", "75a79", "80mais")
+
+#Converte para valores numéricos
+var_classe <- colnames(etario[ , -1])
+etario[var_classe] <- lapply(etario[var_classe], as.numeric)
 
 save(etario, file="Documentos/etario.RData")
 
-#7 - Menores de 25 anos
 
-  # Queremos estudar como o envelhecimento da população tem afetado
-  # A natureza dos processos ajuizados
+#9 - PIB municipal - 2020 e 2021
 
-  # A hipótese é que o número de ações de alimento tem caído à medida que 
-  # O número de menores de 25 anos cai (idade limite em que o direito é assegurado aos filhos)
+# Utilizaremos as estimativas da Fundação Sistema Estadual de Análise de Dados de São Paulo (SEADE)
+# Aplicaremos o incremento percentual anual no PIB do Estado de São Paulo para estimar os valores
+# a seus municípios constituíntes.
 
-  # Por outro lado, talvez as ações de saúde aumentem conforme o número de idosos aumenta
+url <- "https://repositorio.seade.gov.br/dataset/90ae5a90-7638-409c-be21-c3049daa4a55/resource/abf25069-ceeb-4115-8945-e0c4d0508588/download/05-2022_tabelas_site.xlsx"
+  
+GET(url = url, write_disk(path = "Documentos/pib_seade.xlsx", overwrite = TRUE))
 
-# Somando o número de pessoas menores de 25 em cada município
-menor_25 <- etario |>
-  mutate(menor25 = rowSums(etario[, 2:6])) |>
-  select(id, menor25)
+# Desejamos extrair as onformações constantes da planilha nº 9
+excel_sheets(here("Documentos", "pib_seade.xlsx"))
 
-# Criando uma coluna que indica o ano em que cada estimativa foi feita
-menor_25$ano <- as.character(rep(2010:2021, each = 645))
+pib_seade <- as.data.frame(read_excel(here("Documentos", "pib_seade.xlsx"),
+                           sheet = "Mensal 09"))
 
-# Agrupando as observações de cada ano em uma coluna própria
-menor_25 <- tidyr::pivot_wider(menor_25, names_from = ano, values_from = menor25)
+#Selecionando as colunas correspondentes ao mês, ano e PIB
+pib_seade <- pib_seade[ , c(1, 2, 7)] |>
+  rename("mes" = 1, "ano" = 2, "pib" = 3) |>
+  #Selecionando a diferença percentual do PIB dezembro a dezembro
+  filter(ano %in% c("2020", "2021"), mes == "Dezembro")
+
+pib_2020 <- pib |>
+  filter(ano == "2019")|>
+  mutate(pib = round(pib * (1 + pib_seade$pib[1]/100), 0)) |>
+  mutate(ano = "2020")
+
+pib_2021 <- pib_2020|>
+  mutate(pib = round(pib * (1 + pib_seade$pib[2]/100), 0)) |>
+  mutate(ano = "2021")
+
+pib <- rbind(pib, pib_2020, pib_2021)
+
+rm(pib_seade, pib_2020, pib_2021)
+
+save(pib, file="Documentos/pib.RData")
+
+
+################################################################################
+#                            Aglutinando Dados                                 #
+################################################################################
+
+#1 - Agrupando os indicadores em uma mesma tabela
+indicadores <- populacao |>
+  filter(ano %in% 2006:2020) |>
+  left_join(municipios_sp[ , c("id", "comarca")], by = "id") |>
+  left_join(etario, by = c("id",  "ano")) |>
+  left_join(pib, by = c("id",  "ano")) |>
+  left_join(salario, by = c("id",  "ano")) |>
+  left_join(ocupados, by = c("id",  "ano")) |>
+  left_join(idh, by = "id") |>
+  select(id, comarca, everything())
 
 # Verificando a consisTência dos dados
-filter_all(menor_25, any_vars(is.na(.)))
+filter_all(indicadores, any_vars(is.na(.)))
 
-save(menor_25, file="Documentos/menor_25.RData")
+#2 -  Calculando os indicadores para cada ano / comarca
+indicadores <- indicadores |>
+  group_by(ano, comarca)|> 
+  summarise(
+    # Definindo os valores de idh e salário médio das comarcas
+    # pela média ponderada com a populacao de cada cidade em cada ano
+    salario = weighted.mean(salario, populacao),
+    idh = weighted.mean(idh, populacao),
+    # Aplicando a função sum() sobre variáveis que representam
+    # Números absolutos
+    across(-c(id, salario, idh), sum)) |>
+  ungroup () %>% droplevels(.)
 
-#8 - Maiores de 60 anos
-
-# Somando o número de pessoas maiores de 60 em cada município
-maior_60 <- etario |>
-  mutate(maior60 = rowSums(etario[, 14:18])) |>
-  select(id,maior60)
-
-# Criando uma coluna que indica o ano em que cada estimativa foi feita
-maior_60$ano <- as.character(rep(2010:2021, each = 645))
-
-# Agrupando as observações de cada ano em uma coluna própria
-maior_60 <- tidyr::pivot_wider(maior_60, names_from = ano, values_from = maior60)
-
-# Verificando a consisTência dos dados
-filter_all(maior_60, any_vars(is.na(.)))
-
-save(maior_60, file="Documentos/maior_60.RData")
-
+save(indicadores, file="Documentos/indicadores.RData")
